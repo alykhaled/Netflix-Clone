6036d091181d9a9fb3e73b0305c48184c0cb6fa7
Aly Khaled <alykhaled2001@live.com>
2021-07-28 21:47:02 +0200

    Fix the featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { InfoOutlined, PlayArrow } from '@material-ui/icons'
+import './featured.scss'
 
 const Featured = () => {
     return (
-        <div>
+        <div className="featured">
             {/*Get random movies title image in png*/}
             <img src="https://picsum.photos/600" width="100%"alt=""/>
-            <img src="https://www.themoviedb.org/t/p/original/7dQ61kA469sXRrH52HxMOa2KaqD.png" alt="" />
+            <div className="info">
+                <img src="https://www.themoviedb.org/t/p/original/7dQ61kA469sXRrH52HxMOa2KaqD.png" alt="" />
+                <span className="desc">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    In dignissimos laborum nihil atque adipisci, placeat, tenetur porro, quam sed consequatur 
+                    sit dolorum hic aliquid! Reprehenderit maxime accusamus soluta vitae dolor?
+                </span>
+                <div className="buttons">
+                    <button className="play">
+                        <PlayArrow />
+                        <span>Play</span>
+                    </button>
+                    <button className="more">
+                        <InfoOutlined />
+                        <span>Info</span>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
